dea8ebacd5e7ea54e580e8745f70d67cd29318ec
Natalia Jordan <naj46@cornell.edu>
2023-03-08 17:44:20 -0500

    added 404 page

--- weather-app/src/NoMatch.js
+++ weather-app/src/NoMatch.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import four from './404imgp1.jpeg';
+import './NoMatch.css';
+
 
 const NoMatch = () => {
   return (
     <div>
-      <div className="container">
-        <div className="error-page">
-          <h1 className="error-code">404</h1>
-          <p className="error-text">Page not found</p>
-        </div>
+      <h1 className="error-code">404: Page not found</h1>
+      <img className="block-center spacetop" src={four} alt="404 page" id="fourimg" />
+
+      {/* <!-- Source:https://stock.adobe.com/search?k=404+page+not+found&asset_id=326963802 --> */}
+      <p className="cite">Source:<Link to="https://stock.adobe.com/search?k=404+page+not+found&asset_id=326963802"> Adobe Stock </Link> </p>
+      <div className="four">
+        <p > Ohhh nooo, we are sorry. It seems like we can not connect you to the page you are trying to reach. Do not fear, use the link bellow to get back home. Safe travels! </p>
+        <Link to="/">GO BACK HOME</Link>
       </div>
+
     </div>
   );
 };
 
-export default NoMatch;
+export default NoMatch
